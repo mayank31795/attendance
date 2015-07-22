6d2f1dac1503d450d49b7243e5d24945a6fa7095
awaneesh <awanishkishore@gmail.com>
2015-07-22 12:40:55 +0530

    awaneesh commit

---
+++ app/src/main/java/com/example/mj/attendance/FragmentC.java
@@ -0,0 +1,17 @@
+package com.example.mj.attendance;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by AK PC on 22-07-2015.
+ */
+public class FragmentC extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_c,container, false);    }
+}
